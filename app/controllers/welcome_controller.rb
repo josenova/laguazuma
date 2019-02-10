8796727620b33bf472eb7db46161890c22ddf94c
Jose Nova <novajose@gmail.com>
2019-02-10 19:17:33 +0100

    deleted unused route

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,18 +4,4 @@ class WelcomeController < ApplicationController
 		@rooms = Room.all
 	end
 
-	def update
-		@dates = params[:dates]
-
-		Room.each do |r|
-
-			agent = Mechanize.new
-			page = agent.get("https://www.airbnb.com/rooms/#{r.airbnb_id}")
-			form = page.forms.last
-			#page.search(".book-it-panel").text
-
-		end
-
-	end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  get 'search' => 'welcome#update'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
