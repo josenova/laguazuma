6d4b791a00a61e5dc2428e0e8e03c84534189843
Jose Nova <novajose@gmail.com>
2019-02-11 14:19:32 +0100

    room 6 added

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,6 @@ rooms = Room.create([
 	{ number: 2, size: 28, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '14965987', description: "White stone accents in this medium sized room makes this one the coziest of the house. Sit down, relax and enjoy the ocean view in your own private balcony." },
 	{ number: 3, size: 20, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '14769429', description: "Colorful with Dominican roots, this small room is located on a second floor and has a floor-to-ceiling front windows that can be opened for a cool breeze year round." },
 	{ number: 4, size: 20, capacity: 3, beds: 2, bathrooms: 1, airbnb_id: '15048434', description: "This room is only available for rent alongside room #3 as its right under it, which makes them ideal for two couples or groups of five." },
-	{ number: 5, size: 40, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '14796201', description: "Wake up to the most complete view of Samana's Bay. This exotic room has its own Koi fish pond and is equipped with decor from all over the world." }
+	{ number: 5, size: 40, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '14796201', description: "Wake up to the most complete view of Samana's Bay. This exotic room has its own Koi fish pond and is equipped with decor from all over the world." },
+	{ number: 6, size: 32, capacity: 4, beds: 2, bathrooms: 1, airbnb_id: '31733259', description: "Enclosed room equipped with air conditioning and garden views. En-suite bathroom with exterior shower adjacent to the Macaw enclosures." }
 ])
