5371618fc3b95be63a9104cbd2f0d173ef35f61e
Jose Nova <novajose@gmail.com>
2019-06-28 14:02:35 +0200

    Info update

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 rooms = Room.create([
-	{ number: 1, size: 48, capacity: 3, beds: 2, bathrooms: 1, airbnb_id: '12671306', description: "A double bed and a sofa bed makes this room ideal for families of 4. The en-suite bathroom's toilet is strategically positioned to enjoy the view while on duty." }, 
+	{ number: 1, size: 48, capacity: 3, beds: 2, bathrooms: 1, airbnb_id: '12671306', description: "Our most popular room. Equipped with a queen bed and sleeper sofa. The en-suite bathroom's toilet is strategically positioned to enjoy the view while on duty." }, 
 	{ number: 2, size: 28, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '14965987', description: "White stone accents in this medium sized room makes this one the coziest of the house. Sit down, relax and enjoy the ocean view in your own private balcony." },
 	{ number: 3, size: 20, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '14769429', description: "Colorful with Dominican roots, this small room is located on a second floor and has a floor-to-ceiling front windows that can be opened for a cool breeze year round." },
 	{ number: 4, size: 20, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '15048434', description: "This room is only available for rent alongside room #3 as its right under it, which makes them ideal for families of four." },
