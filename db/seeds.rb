36d5c3cd60d49cd8a69bdf0f708438d0bf0c83e7
Jose Nova <novajose@gmail.com>
2019-11-29 17:53:43 +0100

    room 7 seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,6 @@ rooms = Room.create([
 	{ number: 3, size: 20, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '14769429', description: "Painted with Dominican colors, this room is located on a second floor with floor-to-ceiling front windows that can be opened for a cool breeze year round." },
 	{ number: 4, size: 20, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '36661419', description: "Located under room #3, this budget room has a double bed and an en-suite bathroom with shower jets." },
 	{ number: 5, size: 40, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '14796201', description: "Wake up to the most complete view of Samana's Bay. This exotic room has its own Koi fish pond and is equipped with African decor." },
-	{ number: 6, size: 32, capacity: 4, beds: 2, bathrooms: 1, airbnb_id: '31733259', description: "Enclosed room equipped with air conditioning and garden views. En-suite bathroom with exterior shower adjacent to the Macaw enclosures." }
+	{ number: 6, size: 32, capacity: 4, beds: 2, bathrooms: 1, airbnb_id: '31733259', description: "Enclosed room equipped with air conditioning and garden views. En-suite bathroom with exterior shower adjacent to the Macaw enclosures." },
+	{ number: 7, size: 50, capacity: 2, beds: 1, bathrooms: 1, airbnb_id: '40026090', description: "Our latest addition: plenty of space, king bed, romantic morrocan decor and a private hot tub with infinity views." }
 ])
