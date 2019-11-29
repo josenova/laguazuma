ec6dd4b9efe68b7fc0e4c93ff2a7c4c06012cb14
Jose Nova <novajose@gmail.com>
2019-11-29 16:53:37 +0100

    auto loading images

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,8 @@
 class Room < ActiveRecord::Base
+
+  def getImages
+    fullPath = Dir.glob("app/assets/images/rooms/#{self.id}/*.jpg")
+    logger.info fullPath
+    images = fullPath.map{|path| path.split('/').last}.reverse!
+  end
 end
