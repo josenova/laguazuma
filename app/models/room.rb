1052d836df776609b213918e99ad74490f925eea
Jose Nova <novajose@gmail.com>
2019-11-29 17:25:58 +0100

    fix room photo order

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,6 @@ class Room < ActiveRecord::Base
   def getImages
     fullPath = Dir.glob("app/assets/images/rooms/#{self.id}/*.jpg")
     logger.info fullPath
-    images = fullPath.map{|path| path.split('/').last}.reverse!
+    images = fullPath.map{|path| path.split('/').last.split('.').first}.sort
   end
 end
